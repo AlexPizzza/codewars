f07faec681ffc4bca3be40e49885c7a9643739bf
Pita Alexandru <alexandru.pita@nextup.ro>
2022-08-01 16:03:52 +0300

    fix online rpg player to qualifying stage

--- 8kyu/online_rpg_player_to_qualifying_stage.js
+++ 8kyu/online_rpg_player_to_qualifying_stage.js
@@ -1,5 +1,5 @@
 function playerRankUp(points) {
-  return point < 100
+  return points < 100
     ? false
     : "Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up.";
 }
